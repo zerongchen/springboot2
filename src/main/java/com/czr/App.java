5af5c6de331a8edf0d8f3d5d8c88bf9bf2dce7ed
chenzr <chenzr@agilean.cn>
2019-02-20 16:43:24 +0800

    build

---
+++ src/main/java/com/czr/App.java
@@ -0,0 +1,13 @@
+package com.czr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
